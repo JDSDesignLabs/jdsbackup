8f4af8a40b7bd07e79121fb380ea65f9b83735da
JDSDesignLabs <jdsadmin@jdsdesignlabs.com>
2025-06-09 20:38:58 -0400

    loop update

--- backup.py
+++ backup.py
@@ -6,7 +6,6 @@ import shutil as backup
 def menu():
     option_provided = False
     menu_items = ['Exit', 'Standard Backup', 'Backup/Compress', ]
-    print(len(menu_items))
     print("JDS Designs - Backup Script v1")
 
     i = 0
@@ -33,70 +32,53 @@ def standard_backup(src, dst):
     try:
         backup.copyfile(src, dst)
     except FileNotFoundError:
-        print("Sorry... file not found")
+        print("File not found")
+
+
 
 def get_source():
+    valid_path = False
+    while not valid_path:
     #  ask user for file they would like to back up and store in variable a
-    a = input("Please enter the location of the file you would like to back up: ")
-    # convert to path object to be checked by Path
-    path = Path(a)
-    
-    # determine if it is file
-    if path.is_file():
-        is_right_file = 0
-        while is_right_file == 0:
-            # Ask user to confirm backup to validate it is the correct file
-            user_selection = input(f"Please confirm if you would like to backup file:  {path}  (Y/N):")
-            # if the user confirms its the correct file
-            # assign flag to 1
-            if user_selection == "Y":
-                is_right_file = 1
-            # else print please retry and loop
-            else:
-                user_selection = input("Would you like to (R)etry or (E)xit to main menu?(R/E): ")
-                if user_selection == 'E':
-                    return()
+        a = input("Please enter the location of the file you would like to back up: ")
+        # convert to path object to be checked by Path
+        path = Path(a)
+            # determine if it is file
+        if path.is_file():
+            is_right_file = 0
+            while is_right_file == 0:
+                # Ask user to confirm backup to validate it is the correct file
+                user_selection = input(f"Please confirm if you would like to backup file:  {path}  (Y/N):")
+                # if the user confirms its the correct file
+                    # assign flag to 1
+                if user_selection == "Y":
+                    is_right_file = 1
+                    valid_path = True
+                    # else print please retry and loop
+                else:
+                    user_selection = input("Would you like to (R)etry or (E)xit to main menu?(R/E): ")
+                    if user_selection == 'E':
+                        return()
 
-    else:
-        # displays to user that file provided was incorrect
-        print("Invalid file") 
+        else:
+                # displays to user that file provided was incorrect
+            print("Invalid file") 
 
     return(path)   
 
-def get_dst():
-        #  ask user for file they would like to back up and store in variable a
-    a = input("Please enter the location you would like to back up to: ")
-    # convert to path object to be checked by Path
-    path = Path(a)
-    
-    # determine if it is file
-    if path.is_dir():
-        is_right_file = 0
-        while is_right_file == 0:
-            # Ask user to confirm backup to validate it is the correct file
-            user_selection = input(f"Please confirm if you would like to backup file to location:  {path}  (Y/N):")
-            # if the user confirms its the correct file
-            # assign flag to 1
-            if user_selection == "Y":
-                is_right_file = 1
-            # else print please retry and loop
-            else:
-                user_selection = input("Would you like to (R)etry or (E)xit to main menu?(R/E): ")
-                if user_selection == 'E':
-                    return()
-
-    else:
-        # displays to user that file provided was incorrect
-        print("Invalid directory") 
-
-    return path
+def get_dst(src):
+    dst =  str(src) + '.backup'
+    print(dst)
+    user_selection = input("Would you like to set a custom backup location?(Y/N): ")
+    if user_selection == 'Y':
+        print("Please enter")
+    elif user_selection == 'N':
+        return dst
 
 ## Backup data logic -- this is the logic that will ask the user for input prior to calling the backup function and sending the required file path
 def standard_backup_setup():
     src = get_source()
-    dst = get_dst()
-    print(src)
-    print(dst)
+    dst = get_dst(src)
     standard_backup(src, dst)
 
 ## implement compression backup - in progress
