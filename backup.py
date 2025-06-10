48792cea8483177bd0b895cfd4d4f6b8bc6e262d
JDSDesignLabs <jdsadmin@jdsdesignlabs.com>
2025-06-10 17:43:16 -0400

    added working gui

--- backup.py
+++ backup.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 import shutil as backup
 from tkinter import *
-from tkinter import ttk
-
-
+from tkinter import ttk 
 
 def menu():
     option_provided = False
@@ -34,10 +32,10 @@ def standard_backup(src, dst):
     try:
         backup.copyfile(src, dst)
         print("Backup Succcessful")
+        return(True)
     except FileNotFoundError:
         print("File not found")
-
-
+        return(False)
 
 def get_source():
     valid_path = False
@@ -120,8 +118,61 @@ def compress_backup():
     print("You chose to backup and compress")
 
 
+class BackupSystem:
+    def __init__(self, root):
+            root.title("JDS Backup")
+            # create proper padding for grid
+            mainframe = ttk.Frame(root, padding="3 3 12 12")
+            mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
+            root.columnconfigure(0, weight=1)
+            root.rowconfigure(0, weight=1)
+            
+            # First text box for source file entry
+            backup_src = StringVar()
+            backup_src_entry = ttk.Entry(mainframe, width=7, textvariable=backup_src)
+            backup_src_entry.grid(column=3, row=1, sticky=(W,E))
+
+            # Second text box for source file entry
+            backup_dst = StringVar()
+            backup_dst_entry = ttk.Entry(mainframe, width=7, textvariable=backup_dst)
+            backup_dst_entry.grid(column=3, row=2, sticky=(W,E))
+
+            # Shows the provided file location given exists
+            backup_complete=StringVar()
+            ttk.Label(mainframe, textvariable=backup_complete).grid(column=4, row=1, sticky=(W,E))
+            
+            # Shows source label
+            ttk.Label(mainframe, text="Source: ").grid(column=1, row=1, sticky=W)
+            ttk.Label(mainframe, text="Destination: ").grid(column=1, row=2, sticky=W)
+            
+            ttk.Button(mainframe, text="Backup", command=lambda: standard_backup(backup_src.get(), backup_dst.get())).grid(column=3, row=3, sticky=W)
+
+            
+            for child in mainframe.winfo_children():
+                child.grid_configure(padx=5, pady=5)
+            backup_src_entry.focus()
+            root.bind("<Return>", backup)
+
+            # Backup Logic
+            def standard_backup(src, dst):
+                print("Backup in progress...")
+            # handle the error returned if specified file is not found    
+                try:
+                    backup.copyfile(src, dst)
+                    backup_complete.set("Backup Complete.")
+                except FileNotFoundError:
+                    backup_complete.set("File Not Found.")
+
+# set up the window
+root=Tk()
+BackupSystem(root)
+root.mainloop()      
+
 
 def main():
+    # Call window creation command
+    #create_window()
+
     # execute code via terminal window to test
     running = True
     while running:
